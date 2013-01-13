7a0fc082f87d1d6182f49cc3ccea7c372d7cce6d
Jim Bigelow <jim.bigelow.swe@gmail.com>
2013-01-13 09:03:18 -0800

    Base scaffolding for Jim Bigelow's resume

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JimBigelowResume::Application.config.secret_token = '30acb8839485a9f52c095a12c925a7d922bc445f2a5e58cedbd5f0a052587741cf81a606dc085b0ea2216ebf9ad1480b5c87d6ebf19e1c4c6443ef620180aa25'
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+JimBigelowResume::Application.routes.draw do
+
+  get "home/index"
+
+  root :to => 'home#index'
+
+end
